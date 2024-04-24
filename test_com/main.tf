1c1dc513cd00f91444ddd2a001a51c9693fec433
Viral Patel <155760764+VPPATEL-SPS@users.noreply.github.com>
2024-04-23 22:07:39 -0400

    Update TF file: Remove records record

--- test_com/main.tf
+++ test_com/main.tf
@@ -7,15 +7,6 @@ resource "aws_route53_record" "example_A" {
       "192.20.3.2"
     ]
 }
-resource "aws_route53_record" "example101_A" {
-    zone_id = "your_zone_id"
-    name    = "example101.new.com"
-    type    = "A"
-    alias {
-        name = "singam.com"
-        zone_id = "123ASDASDASDASD"
-        evaluate_target_health = false
-    }
 }
 resource "aws_route53_record" "example103_A" {
     zone_id = "your_zone_id"
