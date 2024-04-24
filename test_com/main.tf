e0f6b07ab9d6ee78f059c9f879c3f084ccd9e0a8
Viral Patel <155760764+VPPATEL-SPS@users.noreply.github.com>
2024-04-23 22:04:32 -0400

    Update TF file: Add records record

--- test_com/main.tf
+++ test_com/main.tf
@@ -36,3 +36,12 @@ resource "aws_route53_record" "example101_A" {
       "192.20.3.2"
     ]
 }
+resource "aws_route53_record" "example112_A" {
+    zone_id = "your_zone_id"
+    name    = "example112.new.com"
+    type    = "A"
+    ttl     = "400"
+    records = [
+      "192.20.3.2"
+    ]
+}
