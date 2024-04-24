d456dcfa8c90732c5a9cb5dfb3441c283f21e104
Viral Patel <155760764+VPPATEL-SPS@users.noreply.github.com>
2024-04-23 21:31:53 -0400

    Update TF file: Add alias record

--- test_com/main.tf
+++ test_com/main.tf
@@ -44,3 +44,13 @@ resource "aws_route53_record" "example_A" {
       "192.20.3.2"
     ]
 }
+resource "aws_route53_record" "example101_A" {
+    zone_id = "your_zone_id"
+    name    = "example101.new.com"
+    type    = "A"
+    alias {
+        name = "ewrewr"
+        zone_id = "ewrewr"
+        evaluate_target_health = false
+    }
+}
