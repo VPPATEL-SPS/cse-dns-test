8d3319ca84c88fe42763b0e358467d03c1f305ea
Viral Patel <155760764+VPPATEL-SPS@users.noreply.github.com>
2024-04-23 21:33:31 -0400

    Update TF file: Remove records record

--- test_com/main.tf
+++ test_com/main.tf
@@ -35,15 +35,6 @@ resource "aws_route53_record" "test_A" {
       "192.168.3.2"
     ]
 }
-resource "aws_route53_record" "example_A" {
-    zone_id = "your_zone_id"
-    name    = "example.new.com"
-    type    = "A"
-    ttl     = "400"
-    records = [
-      "192.20.3.2"
-    ]
-}
 resource "aws_route53_record" "example101_A" {
     zone_id = "your_zone_id"
     name    = "example101.new.com"
